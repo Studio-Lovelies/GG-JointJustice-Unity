6dd97d5ad4c1ad135faace3084e8b4d16f0827cb
Bas Mentjot <bmentjot@gmail.com>
2021-08-15 21:09:58 +0200

    Added implementations of the controller interfaces to be extended with actual functionality

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour, IAudioController
+{
+    [Tooltip("Attach the action decoder object here")]
+    [SerializeField] DirectorActionDecoder _directorActionDecoder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_directorActionDecoder == null)
+        {
+            Debug.LogError("Audio Controller doesn't have a action decoder to attach to");
+        }
+        else
+        {
+            _directorActionDecoder.SetAudioController(this);
+        }
+    }
+
+    public void PlayBGSong(string Song)
+    {
+        Debug.LogWarning("PlayBGSong not implemented");
+    }
+
+    public void PlaySFX(string SFX)
+    {
+        Debug.LogWarning("PlaySFX not implemented");
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour, ISceneController
+{
+    [Tooltip("Attach the action decoder object here")]
+    [SerializeField] DirectorActionDecoder _directorActionDecoder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_directorActionDecoder == null)
+        {
+            Debug.LogError("Scene Controller doesn't have a action decoder to attach to");
+        }
+        else
+        {
+            _directorActionDecoder.SetSceneController(this);
+        }
+
+    }
+
+    public void FadeIn(float seconds)
+    {
+        Debug.LogWarning("FadeIn not implemented");
+    }
+
+    public void FadeOut(float seconds)
+    {
+        Debug.LogWarning("FadeOut not implemented");
+    }
+
+    public void PanCamera(float seconds, int xEnd, int yEnd)
+    {
+        Debug.LogWarning("PanCamera not implemented");
+    }
+
+    public void SetBackground(string background)
+    {
+        Debug.LogWarning("SetBackground not implemented");
+    }
+
+    public void SetCameraPos(int x, int y)
+    {
+        Debug.LogWarning("SetCameraPos not implemented");
+    }
+
+    public void ShakeScreen(float intensity)
+    {
+        Debug.LogWarning("ShakeScreen not implemented");
+    }
+}
