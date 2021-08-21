008f0f5adacfe8d9d3473da7bd9c7711cbf6cb24
TooManyMoths <toomanymoths1@gmail.com>
2021-08-21 02:17:16 +0100

    Updated and added tests for 2D menu navigation.

--- Assets/Scripts/SceneLoading/FadeToImageController.cs
+++ Assets/Scripts/SceneLoading/FadeToImageController.cs
@@ -4,21 +4,35 @@ using UnityEngine.UI;
 
 /// <summary>
 /// TransitionController that fades to an image.
-/// Attach this to a game object with an Image component.
+/// Requires an Image component to function. If no Image component is present
+/// one will be created automatically.
 /// </summary>
+[RequireComponent(typeof(Image))]
 public class FadeToImageController : TransitionController
 {
-    [SerializeField, Tooltip("Time taken to fade")]
-    private float _fadeTime;
-
-    [SerializeField, Tooltip("The animation curve used when animating the fadeout. Can be adjusted to modify the animation")]
-    private AnimationCurve _fadeAnimationCurve;
+    [SerializeField, Tooltip("Drag a Fade To Image Settings instance here for the controller to use")]
+    private FadeToImageSettings _fadeToImageSettings;
 
     private Image _image;
+    private float _fadeTime;
+    private AnimationCurve _fadeAnimationCurve;
     
+    /// <summary>
+    /// Unpack the fading settings and get all required components.
+    /// </summary>
     private void Awake()
     {
         _image = GetComponent<Image>();
+
+        if (_fadeToImageSettings == null)
+        {
+            Debug.LogError($"{this} missing FadeToImageSettings instance", this);
+        }
+            
+        _image.sprite = _fadeToImageSettings.Sprite;
+        _image.color = _fadeToImageSettings.Color;
+        _fadeTime = _fadeToImageSettings.Time;
+        _fadeAnimationCurve = _fadeToImageSettings.AnimationCurve;
     }
     
     /// <summary>
@@ -31,6 +45,7 @@ public class FadeToImageController : TransitionController
         imageColor.a = 0;
         _image.color = imageColor;
         yield return StartCoroutine(FadeImage(_image, 0, 1, _fadeTime));
+        yield return null;
     }
     
     /// <summary>
---
+++ Assets/Tests/EditModeTests/MenuNavigatorTests.cs
@@ -0,0 +1,115 @@
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+
+public class MenuNavigatorTests
+{
+    private MockHighlightableMenuItem[] _mockHighlightableMenuItems;
+    private MenuNavigator _menuNavigator;
+
+    private void Setup()
+    {
+        _mockHighlightableMenuItems = new MockHighlightableMenuItem[]
+        {
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem(),
+            new MockHighlightableMenuItem()
+        };
+        _menuNavigator = new MenuNavigator(new Vector2Int(3, 0), 2, _mockHighlightableMenuItems.ToArray<IHightlightableMenuItem>());
+    }
+    
+    [Test, Order(0)]
+    public void IncrementColumn50()
+    {
+        Setup();
+        IncrementTest(50, new Vector2Int(1, 0));
+        Assert.AreEqual(new Vector2Int(3, 0), _menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(1)]
+    public void DecrementColumn3()
+    {
+        IncrementTest(3, new Vector2Int(-1, 0));
+        Assert.AreEqual(new Vector2Int(0, 0),_menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(2)]
+    public void DecrementColumn20()
+    {
+        IncrementTest(20, new Vector2Int(1, 0));
+        Assert.AreEqual(new Vector2Int(0, 0), _menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(3)]
+    public void CheckColumnIncrements()
+    {
+        for (int i = 0; i < _menuNavigator.ColumnCount; i++)
+        {
+            Assert.AreEqual(new Vector2Int(i, 0), _menuNavigator.CurrentPosition);
+            _menuNavigator.IncrementPositionByVector(Vector2Int.right);
+        }
+        Assert.AreEqual(new Vector2Int(0, 0), _menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(4)]
+    public void CheckColumnDecrements()
+    {
+        _menuNavigator.IncrementPositionByVector(Vector2Int.left);
+        for (int i = _menuNavigator.ColumnCount - 1; i >= 0; i--)
+        {
+            Assert.AreEqual(new Vector2Int(i, 0), _menuNavigator.CurrentPosition);
+            _menuNavigator.IncrementPositionByVector(Vector2Int.left);
+        }
+        Assert.AreEqual(new Vector2Int(4, 0), _menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(5)]
+    public void IncrementColumn2()
+    {
+        IncrementTest(2, new Vector2Int(1, 0));
+        Assert.AreEqual(new Vector2Int(1, 0),_menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(6)]
+    public void IncrementRow40()
+    {
+        IncrementTest(40, new Vector2Int(0, 1));
+        Assert.AreEqual(new Vector2Int(1, 0),_menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(7)]
+    public void DecrementRow33()
+    {
+        IncrementTest(33, new Vector2Int(0, 1));
+        Assert.AreEqual(new Vector2Int(1, 1),_menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(8)]
+    public void IncrementColumn1IncrementRow1()
+    { 
+        IncrementTest(1, new Vector2Int(1, 1));
+        Assert.AreEqual(new Vector2Int(2, 0),_menuNavigator.CurrentPosition);
+    }
+    
+    [Test, Order(9)]
+    public void IncrementColumn3IncrementRow5()
+    {
+        IncrementTest(1, new Vector2Int(3, 5));
+        Assert.AreEqual(new Vector2Int(0, 1),_menuNavigator.CurrentPosition);
+    }
+    
+    private void IncrementTest(int repetitions, Vector2Int increment)
+    {
+        for (int i = 0; i < repetitions; i++)
+        {
+            _menuNavigator.IncrementPositionByVector(increment);
+        }
+    }
+}
