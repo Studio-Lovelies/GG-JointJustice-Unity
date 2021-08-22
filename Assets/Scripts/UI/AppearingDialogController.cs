b0f32e92fd642f1fdd1e5750eeb3eeb90d0ed288
Severi <severi.jokipera@gmail.com>
2021-08-22 16:47:09 +0300

    More complex text appear
    Has own information for different situations like letter, dialog, overall, and punctuation.
    Includes separating commands from dialog, but it was disabled for now

--- Assets/Scripts/UI/AppearingDialogController.cs
+++ Assets/Scripts/UI/AppearingDialogController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Events;
@@ -12,6 +13,8 @@ public class AppearingDialogController : MonoBehaviour
     private TextMeshProUGUI _controlledText = null;
     [SerializeField, Tooltip("Default waiting time for all letters, if no alterations have been made in dialog."), Min(0f)]
     private float _defaultAppearTime = 0;
+    [SerializeField, Tooltip("Default waiting time for punctuation, if no alterations have been made in dialog."), Min(0f)]
+    private float _defaultPunctuationAppearTime = 0;
     [SerializeField, Tooltip("When player is giving correct input to the game, how much faster should the game go.")]
     private float _speedMultiplierFromPlayerInput = 2;
 
@@ -32,6 +35,22 @@ public class AppearingDialogController : MonoBehaviour
     private bool _writingDialog = false;
     private bool _initialSetupDone = false;
     private bool _playerIsGivingInput = false;
+    private Dictionary<WaiterTypes, WaitInformation> _allWaiters = new Dictionary<WaiterTypes, WaitInformation>();
+    private Dictionary<int, string> _allDialogCommands = new Dictionary<int, string>();
+    private const char _commandCharacter = '@';
+
+    ///<summary>
+    ///Contains all different special actions used in code. Order matters, as topmost value is most important, and lowest least important.
+    ///</summary>
+    public enum WaiterTypes
+    {
+        letter,
+        punctuation,
+        defaultPunctuation,
+        dialog,
+        overall,
+        defaultValue,
+    }
 
     ///<summary>
     ///Awake is called automatically in the game when object is created, before Start-function.
@@ -61,6 +80,19 @@ public class AppearingDialogController : MonoBehaviour
             return;
         }
 
+        foreach (WaiterTypes wt in (WaiterTypes[])System.Enum.GetValues(typeof(WaiterTypes)))
+        {
+            bool useWithPunctuations = wt == WaiterTypes.punctuation ||
+            wt == WaiterTypes.defaultPunctuation ||
+            wt == WaiterTypes.letter;
+
+            _allWaiters.Add(wt, new WaitInformation(_defaultAppearTime));
+        }
+
+        _allWaiters[WaiterTypes.letter].useWithEverything = true;
+        _allWaiters[WaiterTypes.punctuation].useOnlyWithPunctuations = true;
+        _allWaiters[WaiterTypes.defaultPunctuation].waitTime = _defaultPunctuationAppearTime;
+
         //If the textbox is using autosize, lets take if off or else the text will keep changing size when more dialog appears. 
         _controlledText.autoSizeTextContainer = false;
         _currentAppearTime = _defaultAppearTime;
@@ -73,6 +105,8 @@ public class AppearingDialogController : MonoBehaviour
     private void StartDialogSetup(string dialog)
     {
         _currentDialog = dialog;
+        //Diabled prototype. Take out from comment to enable.
+        //_currentDialog = ReadCommands(dialog);
         _currentLetterNum = 0;
         _writingDialog = true;
         _controlledText.text = _currentDialog;
@@ -159,7 +193,18 @@ public class AppearingDialogController : MonoBehaviour
             return;
         }
 
+        //Diabled prototype. Take out from comment to enable.
+        //CheckCommands();
+
         _currentLetterNum++;
+
+        WaiterTypes newWaiter = GetCurrentWaiter(_currentDialog[_currentLetterNum - 1]);
+
+        if (newWaiter == WaiterTypes.letter)
+            _allWaiters[WaiterTypes.letter].inUse = false;
+
+        _currentAppearTime = _allWaiters[newWaiter].waitTime;
+
     }
 
     ///<summary>
@@ -169,5 +214,246 @@ public class AppearingDialogController : MonoBehaviour
     public void PlayerInput(bool buttonIsDown)
     {
         _playerIsGivingInput = buttonIsDown;
+    }
+
+    ///<summary>
+    ///Checks all different conditions to see which timer to use
+    ///</summary>
+    ///<params name = "characterToAppwar">What is the next character we are checking the timer for. Mainly used to see if its character or punctuation</params>
+    ///<returns>Returns which kind of waiter is used for the next character</returns>
+    private WaiterTypes GetCurrentWaiter(char characterToAppear)
+    {
+        bool isPunctuation = !char.IsLetterOrDigit(characterToAppear) && characterToAppear != ' ';
+
+        foreach (WaiterTypes wt in _allWaiters.Keys)
+        {
+            if ((_allWaiters[wt].useWithEverything ||
+            _allWaiters[wt].useOnlyWithPunctuations == isPunctuation) &&
+            _allWaiters[wt].inUse)
+                return wt;
+        }
+
+        return isPunctuation ? WaiterTypes.defaultPunctuation : WaiterTypes.defaultValue;
+    }
+
+    ///<summary>
+    ///Clears all waiters, so the default is used.
+    ///</summary>
+    public void ClearAllWaiters()
+    {
+        foreach (WaitInformation wi in _allWaiters.Values)
+        {
+            wi.inUse = false;
+        }
+    }
+
+    ///<summary>
+    ///Checks how long the number continues from the start for string.
+    ///</summary>
+    /// <param name="dialogText">The dialog we check the number from.</param>
+    /// <param name="previousTime">How fast the dialog was going previously, in case the number should increase or decrease the current speed.</param>
+    /// <param name="newAppearTime">Gives out the new float containing new appear time rule.</param>
+    /// <returns>Returns true if numeric value was retrieved, or false it there was an error or the command didn't contain numeric value.</returns>
+    static private bool GetFloatFromString(string dialogText, float previousTime, out float newAppearTime)
+    {
+        newAppearTime = 0;
+        //String gathering the information converted to float.
+        string toFloat = "";
+        //The current letters position being examined.
+        int currentLetter = 0;
+        //If the string starts with either + or -, this value gotten from it will be used to increase or decreaset he current speed.
+        bool increase = dialogText[0] == '+';
+        bool decrease = dialogText[0] == '-';
+
+        //If either increase or decrease were found, it will be cut from number conversion. 
+        if (increase || decrease)
+            dialogText = dialogText.Substring(1);
+
+        //The code will check letter by letter if the number continues, or has decimal point/comma.
+        while (currentLetter < dialogText.Length)
+        {
+            char currentCharacter = dialogText[currentLetter];
+
+            if (char.IsDigit(currentCharacter) || currentCharacter == '.' || currentCharacter == ',')
+                toFloat += currentCharacter;
+            else
+                break;
+
+            currentLetter++;
+        }
+
+        //If the string containing all checked letters length is 0, then conversion to number is impossible and rest of the code is ignored.
+        if (toFloat.Length == 0)
+        {
+            Debug.LogWarning("After given command a number was expected, but was not found. Please fix.");
+            return false;
+        }
+
+        float f = float.Parse(toFloat);
+
+        if (increase)
+            f += previousTime;
+        else if (decrease)
+            f = previousTime - f;
+
+        newAppearTime = f;
+        return true;
+    }
+
+    ///<summary>
+    ///Read the commands from dialog and seperates them. Prototype, currently not in use.
+    ///</summary>
+    ///<params name = "dialog">Current dialog to check the commands from</params>
+    ///<returns>Dialog where commands have been separated</returns>
+    private string ReadCommands(string dialog)
+    {
+        //Clear past commands
+        _allDialogCommands.Clear();
+        bool insideCommand = false;
+        string currentCommand = "";
+        string textWithoutCommands = "";
+        int startCommand = 0;
+
+        for (int i = 0; i < dialog.Length; i++)
+        {
+            //Check if the command character is found
+            if (dialog[i] == _commandCharacter)
+            {
+                insideCommand = !insideCommand;
+
+                if (insideCommand)
+                {
+                    startCommand = i;
+                }
+                else if (!insideCommand)
+                {
+                    _allDialogCommands.Add(textWithoutCommands.Length, currentCommand);
+                    currentCommand = "";
+                }
+            }
+            else if (insideCommand)
+            {
+                //While inside a command, add characters to command string
+                currentCommand += dialog[i];
+            }
+            else
+            {
+                //While outside of a command, add characters to actual dialog string
+                textWithoutCommands += dialog[i];
+            }
+        }
+
+        //If the dialog stops while we are still inside the command,it means it wasn't a command.
+        if (insideCommand)
+        {
+            //Add the character from the mistaken command-character to the end to the actual dialog.
+            textWithoutCommands += dialog.Substring(startCommand);
+        }
+
+        return textWithoutCommands;
+    }
+
+    ///<summary>
+    ///Checks the commands for current dialog. Prototype, currently not in use.
+    ///</summary>
+    private void CheckCommands()
+    {
+        if (!_allDialogCommands.ContainsKey(_currentLetterNum))
+            return;
+
+        string currentCommand = _allDialogCommands[_currentLetterNum];
+        int currentLetterNum = 0;
+        WaiterTypes newType = WaiterTypes.defaultValue;
+
+        //Checking what the next letter is to know what action to take.
+        switch (currentCommand[currentLetterNum])
+        {
+            //L for letter
+            case 'l':
+            case 'L':
+                newType = WaiterTypes.letter;
+                break;
+            //P for punctuation
+            case 'p':
+            case 'P':
+                newType = WaiterTypes.punctuation;
+                break;
+            //D for dialog
+            case 'd':
+            case 'D':
+                newType = WaiterTypes.dialog;
+                break;
+            //A for all
+            case 'a':
+            case 'A':
+                newType = WaiterTypes.overall;
+                break;
+            //C for clear
+            case 'c':
+            case 'C':
+                ClearAllWaiters();
+                break;
+            default:
+                Debug.Log("Command not recognized.");
+                return;
+        }
+
+
+        if (newType != WaiterTypes.defaultValue)
+        {
+            SetTimerValue(newType, currentCommand.Substring(1));
+        }
+    }
+
+    ///<summary>
+    ///Sets timer value to given float.
+    ///</summary>
+    ///<params name = "waiterTypeToChange">What type of waiter should be changed</params>
+    ///<params name = "valueToTurnFloat">string containing the numeric value of new appear time</params>
+    public void SetTimerValue(WaiterTypes waiterTypeToChange, string valueToTurnFloat)
+    {
+        //Check that there was a number after command we could use as new appear time.
+        if (GetFloatFromString(valueToTurnFloat, _allWaiters[waiterTypeToChange].waitTime, out float newAppearTime))
+        {
+            SetTimerValue(waiterTypeToChange, newAppearTime);
+        }
+        else
+        {
+            Debug.LogError("Was expecting numeric value, but command didn't contain it.");
+        }
+    }
+
+
+    ///<summary>
+    ///Sets timer value to given float.
+    ///</summary>
+    ///<params name = "waiterTypeToChange">What type of waiter should be changed</params>
+    ///<params name = "newAppearTime">float value that will be set as the waiters ne appear time.</params>
+    public void SetTimerValue(WaiterTypes waiterTypeToChange, float newAppearTime)
+    {
+        _allWaiters[waiterTypeToChange].inUse = true;
+        _allWaiters[waiterTypeToChange].waitTime = newAppearTime;
+    }
+
+    ///<summary>
+    ///Small class containing if the current WaitTimer is in use, and how long to wait if it is.
+    ///</summary>
+    private class WaitInformation
+    {
+        public WaitInformation() { }
+
+        public WaitInformation(float waitTime)
+        {
+            this.waitTime = waitTime;
+        }
+
+        //Is this WaitInformation in use now.
+        public bool inUse = false;
+        //What is the current wait time of this information.
+        public float waitTime = 0;
+        //Is this information used only with punctuations.
+        public bool useOnlyWithPunctuations = false;
+        //Is this information used with everything.
+        public bool useWithEverything = false;
     }
 }
