77fad7cc29ee98f1927ea2773ae1d642b3d733bc
Severi <severi.jokipera@gmail.com>
2021-08-29 13:35:36 +0300

    Fixed autobuilder error
    The autobuilder was causing error from editor script even though its in the editor folder. To get rid of the script, I put the editor script between #if UNITY_EDITOR so its forced not compile on anything else except editor.

--- Assets/Scripts/Attributes/Editor/ReadOnlyDrawer.cs
+++ Assets/Scripts/Attributes/Editor/ReadOnlyDrawer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
 
 /// <summary>
@@ -25,3 +26,4 @@ public class ReadOnlyDrawer : PropertyDrawer
         GUI.enabled = previousGUIState;
     }
 }
+#endif
