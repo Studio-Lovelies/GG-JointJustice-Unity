a0f44e736cef8e08f8e2cb376bb8a325446d950f
TooManyMoths <toomanymoths1@gmail.com>
2021-09-11 00:26:38 +0100

    Added checking menu information updates when highlighting items to tests

--- Assets/Tests/PlayModeTests/Scripts/EvidenceMenu/EvidenceMenuKeyboardTest.cs
+++ Assets/Tests/PlayModeTests/Scripts/EvidenceMenu/EvidenceMenuKeyboardTest.cs
@@ -1,9 +1,12 @@
 using System.Collections;
+using System.Linq;
 using NUnit.Framework;
 using Tests.PlayModeTests.Tools;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using UnityEngine.UI;
 
 namespace Tests.PlayModeTests.Scripts.EvidenceMenu
 {
@@ -12,6 +15,9 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
         private readonly InputTestTools _inputTestTools = new InputTestTools();
         private global::EvidenceMenu _evidenceMenu;
 
+        /// <summary>
+        /// Attempts to open and close the menu and checks if the menu is active after each attempt.
+        /// </summary>
         [UnityTest, Order(0)]
         public IEnumerator EvidenceMenuOpensAndCloses()
         {
@@ -25,6 +31,10 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
             Assert.False(_evidenceMenu.isActiveAndEnabled);
         }
 
+        /// <summary>
+        /// Attempts to close the menu when evidence is being
+        /// presented then asserts that the menu is still open.
+        /// </summary>
         [UnityTest, Order(1)]
         public IEnumerator EvidenceMenuCannotBeClosedWhenPresentingEvidence()
         {
@@ -34,6 +44,9 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
             Assert.True(_evidenceMenu.isActiveAndEnabled);
         }
 
+        /// <summary>
+        /// Selects evidence and asserts that the menu has closed.
+        /// </summary>
         [UnityTest, Order(2)]
         public IEnumerator EvidenceCanBeSelected()
         {
@@ -41,6 +54,9 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
             Assert.False(_evidenceMenu.isActiveAndEnabled);
         }
         
+        /// <summary>
+        /// Attempts to navigate left and right then asserts that the correct item is selected.
+        /// </summary>
         [UnityTest, Order(3)]
         public IEnumerator CanNavigateWithLeftAndRightArrows()
         {
@@ -55,37 +71,94 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
             Assert.AreEqual("Bent Coins", _evidenceMenu.GetComponent<Menu>().SelectedButton.GetComponent<EvidenceMenuItem>().Evidence.DisplayName);
         }
         
+        /// <summary>
+        /// Presses navigation buttons a lot of times then asserts
+        /// that the selected item is selected.
+        /// </summary>
         [UnityTest, Order(4)]
         public IEnumerator CanNavigateToMultiplePages()
         {
+            // Get to the correct point in the scene
             yield return _inputTestTools.PressForFrame(_inputTestTools.Keyboard.enterKey);
             yield return _inputTestTools.WaitForBehaviourActiveAndEnabled(_evidenceMenu, _inputTestTools.Keyboard.xKey);
             yield return _inputTestTools.PressForFrame(_inputTestTools.Keyboard.enterKey);
             yield return _inputTestTools.WaitForBehaviourActiveAndEnabled(_evidenceMenu, _inputTestTools.Keyboard.xKey);
 
+            // Spam navigation button
             yield return _inputTestTools.PressForFrame(_inputTestTools.Keyboard.leftArrowKey);
             yield return _inputTestTools.RepeatPressForFrames(_inputTestTools.Keyboard.enterKey, 50);
-            yield return _inputTestTools.RepeatPressForFrames(_inputTestTools.Keyboard.rightArrowKey, 10);
+
+            // Get the evidence menu text boxes to check if they are updated correctly
+            Menu menu = _evidenceMenu.GetComponent<Menu>();
+            TextMeshProUGUI[] evidenceTextBoxes = _evidenceMenu.GetComponentsInChildren<TextMeshProUGUI>();
+            TextMeshProUGUI evidenceName =
+                evidenceTextBoxes.First(evidenceTextBoxe => evidenceTextBoxe.gameObject.name == "EvidenceName");
+            TextMeshProUGUI evidenceDescription = evidenceTextBoxes.First(evidenceTextBox =>
+                evidenceTextBox.gameObject.name == "EvidenceDescription");
+            Image[] images = _evidenceMenu.GetComponentsInChildren<Image>();
+            Image evidenceIcon = images.First(image => image.gameObject.name == "EvidenceIcon");
+            
+            // Move to right navigation button and check displayed menu information is updated correctly.
+            for (int i = 0; i < 9; i++)
+            {
+                yield return _inputTestTools.PressForFrame(_inputTestTools.Keyboard.rightArrowKey);
+                ICourtRecordObject evidence = menu.SelectedButton.GetComponent<EvidenceMenuItem>().Evidence;
+                Assert.AreEqual(evidenceName.text, evidence.DisplayName);
+                Assert.AreEqual(evidenceDescription.text, evidence.Description);
+                Assert.AreEqual(evidenceIcon.sprite, evidence.Icon);
+            }
+
+            // Spam navigation button
+            yield return _inputTestTools.PressForFrame(_inputTestTools.Keyboard.rightArrowKey);
             yield return _inputTestTools.RepeatPressForFrames(_inputTestTools.Keyboard.enterKey, 101);
             yield return _inputTestTools.RepeatPressForFrames(_inputTestTools.Keyboard.leftArrowKey, 4);
 
-            yield return null;
-            
+            // After all this Jory Sr's Letter should be selected
             Assert.AreEqual("Jory Sr's Letter",_evidenceMenu.GetComponent<Menu>().SelectedButton.GetComponent<EvidenceMenuItem>().Evidence.DisplayName);
         }
 
+        /// <summary>
+        /// Tries to open the profile menu and checks if the evidence menu items display profiles.
+        /// Closes profile menu at the end and checks something is selected.
+        /// </summary>
         [UnityTest, Order(5)]
         public IEnumerator ProfileMenuCanBeAccessed()
         {
+            // Get the evidence menu text boxes to check if they are updated correctly
+            Menu menu = _evidenceMenu.GetComponent<Menu>();
+            TextMeshProUGUI[] evidenceTextBoxes = _evidenceMenu.GetComponentsInChildren<TextMeshProUGUI>();
+            TextMeshProUGUI evidenceName =
+                evidenceTextBoxes.First(evidenceTextBoxe => evidenceTextBoxe.gameObject.name == "EvidenceName");
+            TextMeshProUGUI evidenceDescription = evidenceTextBoxes.First(evidenceTextBox =>
+                evidenceTextBox.gameObject.name == "EvidenceDescription");
+            Image[] images = _evidenceMenu.GetComponentsInChildren<Image>();
+            Image evidenceIcon = images.First(image => image.gameObject.name == "EvidenceIcon");
+
+            // Check displayed menu information updates properly.
             yield return _inputTestTools.PressForFrame(_inputTestTools.Keyboard.cKey);
-            Assert.AreEqual("Arin", _evidenceMenu.GetComponent<Menu>().SelectedButton.GetComponent<EvidenceMenuItem>().Evidence.DisplayName);
+            ICourtRecordObject evidence = menu.SelectedButton.GetComponent<EvidenceMenuItem>().Evidence;
+            Assert.AreEqual("Arin", evidence.DisplayName);
+            Assert.AreEqual(evidenceName.text, evidence.DisplayName);
+            Assert.AreEqual(evidenceDescription.text, evidence.Description);
+            Assert.AreEqual(evidenceIcon.sprite, evidence.Icon);
             yield return _inputTestTools.RepeatPressForFrames(_inputTestTools.Keyboard.rightArrowKey, 5);
-            Assert.AreEqual("Ross", _evidenceMenu.GetComponent<Menu>().SelectedButton.GetComponent<EvidenceMenuItem>().Evidence.DisplayName);
+            evidence = menu.SelectedButton.GetComponent<EvidenceMenuItem>().Evidence;
+            Assert.AreEqual("Ross", evidence.DisplayName);
+            Assert.AreEqual(evidenceName.text, evidence.DisplayName);
+            Assert.AreEqual(evidenceDescription.text, evidence.Description);
+            Assert.AreEqual(evidenceIcon.sprite, evidence.Icon);
             yield return _inputTestTools.PressForFrame(_inputTestTools.Keyboard.cKey);
-            Assert.AreEqual("Attorney's Badge", _evidenceMenu.GetComponent<Menu>().SelectedButton.GetComponent<EvidenceMenuItem>().Evidence.DisplayName);
+            evidence = menu.SelectedButton.GetComponent<EvidenceMenuItem>().Evidence;
+            Assert.AreEqual("Attorney's Badge", evidence.DisplayName);
+            Assert.AreEqual(evidenceName.text, evidence.DisplayName);
+            Assert.AreEqual(evidenceDescription.text, evidence.Description);
+            Assert.AreEqual(evidenceIcon.sprite, evidence.Icon);
         }
 
-        [UnityTest, Order(5)]
+        /// <summary>
+        /// Checks evidence is correctly substituted with its designated alternate evidence.
+        /// </summary>
+        [UnityTest, Order(6)]
         public IEnumerator EvidenceCanBeSubstitutedWithAltEvidence()
         {
             yield return _inputTestTools.PressForFrame(_inputTestTools.Keyboard.enterKey);
--- Assets/Tests/PlayModeTests/Scripts/EvidenceMenu/EvidenceMenuMouseTest.cs
+++ Assets/Tests/PlayModeTests/Scripts/EvidenceMenu/EvidenceMenuMouseTest.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Linq;
 using NUnit.Framework;
 using Tests.PlayModeTests.Tools;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -17,6 +18,9 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
         private Keyboard Keyboard => _inputTestTools.Keyboard;
         private Mouse Mouse => _inputTestTools.Mouse;
         
+        /// <summary>
+        /// Places mouse over each menu item and asserts it is highlighted.
+        /// </summary>
         [UnityTest, Order(0)]
         public IEnumerator MouseShouldHighlightEvidenceMenuItems()
         {
@@ -24,6 +28,7 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
             yield return null;
             _evidenceMenu = Resources.FindObjectsOfTypeAll<global::EvidenceMenu>()[0];
             
+            // Get to required point in scene
             yield return _inputTestTools.WaitForBehaviourActiveAndEnabled(_evidenceMenu, Keyboard.xKey);
             yield return _inputTestTools.PressForFrame(Keyboard.enterKey);
             yield return _inputTestTools.WaitForBehaviourActiveAndEnabled(_evidenceMenu, Keyboard.xKey);
@@ -32,23 +37,27 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
             yield return _inputTestTools.PressForFrame(Keyboard.enterKey);
             yield return _inputTestTools.WaitForBehaviourActiveAndEnabled(_evidenceMenu, Keyboard.xKey);
 
+            // Get the menu items to test
             MenuItem[] menuItems = GameObject.Find("EvidenceContainer").GetComponentsInChildren<MenuItem>();
-            MenuItem decrementButton = GameObject.Find("DecrementButton").GetComponent<MenuItem>();
-            MenuItem incrementButton = GameObject.Find("IncrementButton").GetComponent<MenuItem>();
             EvidenceMenuItem firstMenuItem = menuItems.First(menuItem => menuItem.gameObject.name == "EvidenceMenuItem").GetComponent<EvidenceMenuItem>();
             yield return _inputTestTools.SetMousePosition(firstMenuItem.transform.position);
             Menu menu = _evidenceMenu.GetComponent<Menu>();
             Assert.AreEqual(firstMenuItem.Evidence.DisplayName, menu.SelectedButton.GetComponent<EvidenceMenuItem>().Evidence.DisplayName);
 
+            // Loop through the menu items and check if they highlight correctly
             for (int i = 1; i < menuItems.Length; i++)
             {
                 MenuItem menuItem = menuItems.First(menuItem => menuItem.gameObject.name == $"EvidenceMenuItem ({i})");
                 yield return _inputTestTools.SetMousePosition(menuItem.transform.position);
-                yield return null;
-                Assert.AreEqual(menuItem.GetComponent<EvidenceMenuItem>().Evidence.DisplayName, menu.SelectedButton.GetComponent<EvidenceMenuItem>().Evidence.DisplayName);
+                ICourtRecordObject evidence = menu.SelectedButton.GetComponent<EvidenceMenuItem>().Evidence;
+                Assert.AreEqual(menuItem.GetComponent<EvidenceMenuItem>().Evidence.DisplayName, evidence.DisplayName);
             }
         }
         
+        /// <summary>
+        /// Presses the navigation buttons multiple times then checks if
+        /// the first menu item is the same as the one that the menu thinks is selected.
+        /// </summary>
         [UnityTest, Order(1)]
         public IEnumerator NavigationButtonsCanBeClicked()
         {
@@ -68,6 +77,9 @@ namespace Tests.PlayModeTests.Scripts.EvidenceMenu
                 menu.SelectedButton.GetComponent<EvidenceMenuItem>().Evidence.DisplayName);
         }
 
+        /// <summary>
+        /// Attempts to click a menu item and asserts the menu is closed.
+        /// </summary>
         [UnityTest, Order(2)]
         public IEnumerator MenuItemsCanBeClickedWithMouse()
         {
