4576879f5f0c3532b70944614103c4f6348aa0f6
TooManyMoths <toomanymoths1@gmail.com>
2021-10-06 21:07:08 +0100

    Screenshake tests written

--- Assets/Tests/EditModeTests/ScreenshakeCalculatorTests.cs
+++ Assets/Tests/EditModeTests/ScreenshakeCalculatorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
@@ -9,15 +10,16 @@ public class ScreenshakeCalculatorTests
     private const float SHAKE_DURATION = 50;
     private const int NUMBER_OF_POSITIONS = (int)(SHAKE_DURATION / DELTA_TIME);
     private const float AMPLITUDE = 0.1f;
+    private readonly Vector2 NOISE_SCALE = new Vector2(1, 1);
 
     private ScreenshakeCalculator _screenshakeCalculator;
-    private float _time;
+    private Vector3[] _shakePositions;
 
     [SetUp]
     public void Setup()
     {
         _screenshakeCalculator = CreateScreenshakeCalculator();
-        _time = 0;
+        _shakePositions = new Vector3[NUMBER_OF_POSITIONS];
     }
 
     [Test]
@@ -38,37 +40,67 @@ public class ScreenshakeCalculatorTests
     [Test]
     public void ScreenShakesWithTheSameValuesAreIdentical()
     {
-        Vector3[] shakePositions = new Vector3[NUMBER_OF_POSITIONS];
         for (int i = 0; i < NUMBER_OF_POSITIONS; i++)
         {
-            shakePositions[i] = _screenshakeCalculator.Calculate(DELTA_TIME);
+            _shakePositions[i] = _screenshakeCalculator.Calculate(DELTA_TIME);
         }
 
         var screenShakeCalculatorComparison = CreateScreenshakeCalculator();
         
         for (int i = 0; i < NUMBER_OF_POSITIONS; i++)
         {
-            Assert.AreEqual(shakePositions[i], screenShakeCalculatorComparison.Calculate(DELTA_TIME));
+            Assert.AreEqual(_shakePositions[i], screenShakeCalculatorComparison.Calculate(DELTA_TIME));
         }
     }
 
     [Test]
     public void ShakeIsConstrainedWithinSpecifiedAmplitude()
     {
-        Vector3[] shakePositions = new Vector3[NUMBER_OF_POSITIONS];
-        Keyframe[] animationKeys = { new Keyframe(0, 1), new Keyframe(1, 1) };
-        var screenshakeCalculator = new ScreenshakeCalculator(SHAKE_DURATION, FREQUENCY, AMPLITUDE, new Vector2(1, 1),
-            new Vector2(0, 0), new AnimationCurve(animationKeys));
+        var animationKeys = new [] { new Keyframe(0, 1), new Keyframe(1, 1) };
+        var screenshakeCalculator = CreateScreenshakeCalculator(animationCurve: new AnimationCurve(animationKeys));
         for (int i = 0; i < NUMBER_OF_POSITIONS; i++)
         {
-            shakePositions[i] = screenshakeCalculator.Calculate(DELTA_TIME);
+            _shakePositions[i] = screenshakeCalculator.Calculate(DELTA_TIME);
         }
         
-        Assert.IsTrue(shakePositions.Max(vector3 => Mathf.Approximately(vector3.x, AMPLITUDE));
+        Assert.IsTrue(_shakePositions.Any(position => position != Vector3.zero));
+        Assert.IsTrue(_shakePositions.All(position => !Mathf.Approximately(position.x,AMPLITUDE) && !Mathf.Approximately(position.x,AMPLITUDE)));
     }
 
-    private ScreenshakeCalculator CreateScreenshakeCalculator()
+    [Test]
+    public void NoShakeWhenAmplitudeIsZero()
     {
-        return new ScreenshakeCalculator(SHAKE_DURATION, FREQUENCY, AMPLITUDE, new Vector2(1, 1), new Vector2(0, 0));
+        var screenshakeCalculator = CreateScreenshakeCalculator(amplitude: 0);
+        for (int i = 0; i < NUMBER_OF_POSITIONS; i++)
+        {
+            
+            _shakePositions[i] = screenshakeCalculator.Calculate(DELTA_TIME);
+        }
+        
+        Assert.IsTrue(_shakePositions.All(position => position == Vector3.zero));
+    }
+
+    [Test]
+    public void NumberOfOscillationsMatchesFrequency()
+    {
+        var animationKeys = new [] { new Keyframe(0, 1), new Keyframe(1, 1) };
+        var screenshakeCalculator = CreateScreenshakeCalculator(noiseScaleX: 0, noiseScaleY: 0, animationCurve: new AnimationCurve(animationKeys));
+        int previousSign = Math.Sign(screenshakeCalculator.Calculate(DELTA_TIME).x);
+        int oscillationCount = 0;
+        for (int i = 0; i < NUMBER_OF_POSITIONS; i++)
+        {
+            int currentSign = Math.Sign(screenshakeCalculator.Calculate(DELTA_TIME).x);
+            if (currentSign != previousSign)
+            {
+                oscillationCount++;
+                previousSign = currentSign;
+            }
+        }
+        Assert.IsTrue(Mathf.Approximately(oscillationCount / SHAKE_DURATION, FREQUENCY * 2));
+    }
+
+    private ScreenshakeCalculator CreateScreenshakeCalculator(float shakeDuration = SHAKE_DURATION, float frequency = FREQUENCY, float amplitude = AMPLITUDE, float noiseScaleX = 1, float noiseScaleY = 1, float noiseOffsetX = 0, float noiseOffsetY = 1, AnimationCurve animationCurve = null)
+    {
+        return new ScreenshakeCalculator(shakeDuration, frequency, amplitude, new Vector2(noiseScaleX, noiseScaleY), new Vector2(noiseOffsetX, noiseOffsetY), animationCurve);
     }
 }
