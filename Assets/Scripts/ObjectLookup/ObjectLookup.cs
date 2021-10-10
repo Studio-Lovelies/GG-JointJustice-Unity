4359b56a0e2218213ad874e4596c040a858bb1c2
TooManyMoths <toomanymoths1@gmail.com>
2021-10-10 01:57:43 +0100

    Reverted IsObjectInList check to a warning for adding objects, as this should not prevent program execution

--- Assets/Scripts/ObjectLookup/ObjectLookup.cs
+++ Assets/Scripts/ObjectLookup/ObjectLookup.cs
@@ -41,7 +41,11 @@ public class ObjectLookup<T> where T : Object
     {
         VerifyObjectInDictionary(objectName);
         T obj = this[objectName];
-        VerifyObjectNotInList(obj);
+        if (IsObjectInList(obj))
+        {
+            Debug.LogWarning($"Could not add object {obj.name} to list as it is already in the list of current objects.");
+            return;
+        }
         _currentObjectList.Add(obj);
     }
 
@@ -100,20 +104,6 @@ public class ObjectLookup<T> where T : Object
         if (!IsObjectInList(obj))
         {
             throw new ArgumentException($"Object {obj.name} could not be found in the list of current objects.");
-        }
-    }
-
-    /// <summary>
-    /// Method to check if an object is NOT in the list of current objects.
-    /// Throws an ArgumentException if the object is in the list.
-    /// </summary>
-    /// <param name="obj"></param>
-    /// <exception cref="ArgumentException"></exception>
-    private void VerifyObjectNotInList(T obj)
-    {
-        if (IsObjectInList(obj))
-        {
-            throw new ArgumentException($"Object {obj.name} is already in the list of current objects.");
         }
     }
 
