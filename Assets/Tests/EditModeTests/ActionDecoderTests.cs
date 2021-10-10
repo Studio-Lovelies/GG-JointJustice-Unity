16e431d70e3df9a844d1cd0e91e1f29f4baf76af
notexplosive <thenotexplosive@gmail.com>
2021-10-09 20:45:40 -0700

    (EXPERIMENTAL) Add mechanism for generating docs

--- Assets/Tests/EditModeTests/ActionDecoderTests.cs
+++ Assets/Tests/EditModeTests/ActionDecoderTests.cs
@@ -166,5 +166,14 @@ namespace Tests.EditModeTests
             Assert.AreEqual("Happy", actorController.emotion);
             Assert.IsFalse(actionDoneCalled);
         }
+
+        [Test]
+        public void DescribeActionArguments()
+        {
+            var decoder = new ActionDecoder();
+            var description = decoder.Describe(ActionName.CAMERA_PAN);
+
+            Assert.AreEqual("CAMERA_PAN takes args duration:decimal number, x:int, y:int", description);
+        }
     }
 }
