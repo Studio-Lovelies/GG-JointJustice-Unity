e73e67722475ed825be42ece0816dda3ae41b7a8
Vincent Mahnke <vincent@mahn.ke>
2021-11-12 23:08:46 +0100

    Removed superfluous Applies() method from Parser Removed IParser

--- Assets/Scripts/TextDecoder/Parser/Parser.cs
+++ Assets/Scripts/TextDecoder/Parser/Parser.cs
@@ -1,14 +1,7 @@
-using System;
-
 namespace TextDecoder.Parser
 {
-    public abstract class Parser<T> : IParser
+    public abstract class Parser<T>
     {
         public abstract string Parse(string input, out T output);
-
-        public bool Applies(Type type)
-        {
-            return typeof(T) == type;
-        }
     }
 }
