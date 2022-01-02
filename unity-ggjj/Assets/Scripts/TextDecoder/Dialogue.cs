c1d63db88afbfe7210c582c5c264ab25310672a0
Vincent Mahnke <vincent@mahn.ke>
2022-01-02 13:36:28 +0100

    Removed unused ability to deduct a "Dialogue"s GameMode on it's own (a regular "&MODE:"-line is parsed by the action decoder now)

--- unity-ggjj/Assets/Scripts/TextDecoder/Dialogue.cs
+++ unity-ggjj/Assets/Scripts/TextDecoder/Dialogue.cs
@@ -1,5 +1,4 @@
 using System;
-using Ink.Runtime;
 using UnityEngine;
 
 [Serializable]
@@ -16,29 +15,4 @@ public struct Dialogue
 
     [field: Tooltip("Drag an Ink narrative script here.")]
     [field: SerializeField] public TextAsset NarrativeScript { get; private set; }
-    
-    public GameMode ScriptMode
-    {
-        get
-        {
-            const string expectedFileStart = "&MODE:";
-            var firstLine = new Story(NarrativeScript.text).Continue().Trim();
-            var availableModes = (GameMode[])Enum.GetValues(typeof(GameMode));
-            if (!firstLine.StartsWith("&MODE:"))
-            {
-                throw new NotSupportedException($"The first line of each .ink script needs to begin with either '{expectedFileStart}{string.Join($"','{expectedFileStart}", availableModes)}'\r\nLine: {firstLine}");
-            }
-
-            var modeParameter = firstLine.Substring(expectedFileStart.Length);
-            foreach (GameMode mode in availableModes)
-            {
-                if (mode.ToString() == modeParameter)
-                {
-                    return mode;
-                }
-            }
-
-            throw new NotSupportedException($"The mode '{modeParameter}' is not supported. (Currently supported values: '{expectedFileStart}{string.Join($"','{expectedFileStart}", availableModes)}')\r\n Line: {firstLine}");
-        }
-    }
 }
