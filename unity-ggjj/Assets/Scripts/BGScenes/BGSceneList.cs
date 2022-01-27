7a9a550a4b4ee0cef272841900e6849d33fe5d91
moths <67964179+PhoebeMitchell@users.noreply.github.com>
2022-01-27 03:26:52 +0000

    Update unity-ggjj/Assets/Scripts/BGScenes/BGSceneList.cs
    Co-authored-by: Vincent Mahnke <accounts@vimaster.de>

--- unity-ggjj/Assets/Scripts/BGScenes/BGSceneList.cs
+++ unity-ggjj/Assets/Scripts/BGScenes/BGSceneList.cs
@@ -54,7 +54,7 @@ public class BGSceneList : MonoBehaviour
     /// </summary>
     /// <param name="sceneName">name of the target scene</param>
     /// <returns>The new active scene. Can be null if an error occurred.</returns>
-    public BGScene SetScene(SceneAssetName sceneName) //TODO: Change this when making file names universal
+    public BGScene SetScene(SceneAssetName sceneName)
     {
         if (!_bgScenes.ContainsKey(sceneName))
         {
