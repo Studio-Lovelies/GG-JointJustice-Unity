5ce5783d6b940e7c8c9e97727c72ad477f5636db
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-02-07 07:13:05 +0000

    Implemented random splashes

---
+++ unity-ggjj/Assets/Scripts/Menu/SplashScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    [SerializeField] private Sprite _alternateSprite;
+    [SerializeField] private float _duration;
+    [SerializeField] private SceneLoader _sceneLoader;
+    
+    private IEnumerator Start()
+    {
+        if (Random.Range(0f, 1f) < 0.05f)
+        {
+            Debug.Log("random");
+            GetComponent<SpriteRenderer>().sprite = _alternateSprite;
+        }
+
+        yield return new WaitForSeconds(_duration);
+        _sceneLoader.LoadScene("MainMenu");
+    }
+}
