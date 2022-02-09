1a845fb815bfa62c3dc8aa5454aaa1ba31491e1c
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-02-09 19:25:35 +0000

    Moved NarrativeCase to its own file

--- unity-ggjj/Assets/Scripts/Menu/CaseSelectMenu.cs
+++ unity-ggjj/Assets/Scripts/Menu/CaseSelectMenu.cs
@@ -44,11 +44,3 @@ public class CaseSelectMenu : MonoBehaviour
         }
     }
 }
-
-[Serializable]
-public struct NarrativeCase
-{
-    [field: SerializeField] public string Name { get; private set; }
-    [field: SerializeField] public Sprite PreviewImage { get; private set; }
-    [field: SerializeField] public TextAsset[] Chapters { get; private set; }
-}
