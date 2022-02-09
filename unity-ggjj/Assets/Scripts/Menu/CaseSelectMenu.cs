518f12573bb4079534348c7dc8b75bbf6976618f
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-02-09 19:17:47 +0000

    Changed Case to NarrativeCase to remove @case

--- unity-ggjj/Assets/Scripts/Menu/CaseSelectMenu.cs
+++ unity-ggjj/Assets/Scripts/Menu/CaseSelectMenu.cs
@@ -8,7 +8,7 @@ public class CaseSelectMenu : MonoBehaviour
     [SerializeField] private Transform _buttonContainer;
     [SerializeField] private Image _previewImage;
     [SerializeField] private ChapterSelectMenu _chapterSelectMenu;
-    [SerializeField] private Case[] _cases;
+    [SerializeField] private NarrativeCase[] _cases;
 
     /// <summary>
     /// On Awake:
@@ -24,23 +24,23 @@ public class CaseSelectMenu : MonoBehaviour
         backButton.OnItemSelect.AddListener(() => _previewImage.color = Color.black);
         backButton.OnItemDeselect.AddListener(() => _previewImage.color = Color.white);
 
-        foreach (var @case in _cases)
+        foreach (var narrativeCase in _cases)
         {
             var menuItem = Instantiate(_buttonPrefab, _buttonContainer);
-            menuItem.Text = @case.Name;
-            menuItem.OnItemSelect.AddListener(() => _previewImage.sprite = @case.PreviewImage);
+            menuItem.Text = narrativeCase.Name;
+            menuItem.OnItemSelect.AddListener(() => _previewImage.sprite = narrativeCase.PreviewImage);
             var menuOpener = menuItem.GetComponent<MenuOpener>();
             menuOpener.MenuToOpen = _chapterSelectMenu.Menu;
             ((Button)menuItem.Selectable).onClick.AddListener(() =>
             {
-                if (@case.Chapters.Length == 1)
+                if (narrativeCase.Chapters.Length == 1)
                 {
                     // No need to show chapters, put logic for starting game here
                 }
                 else
                 {
                     menuOpener.OpenMenu();
-                    _chapterSelectMenu.Initialise(@case.Chapters, menuOpener);
+                    _chapterSelectMenu.Initialise(narrativeCase.Chapters, menuOpener);
                 }
             });
         }
@@ -48,7 +48,7 @@ public class CaseSelectMenu : MonoBehaviour
 }
 
 [Serializable]
-public struct Case
+public struct NarrativeCase
 {
     [field: SerializeField] public string Name { get; private set; }
     [field: SerializeField] public Sprite PreviewImage { get; private set; }
