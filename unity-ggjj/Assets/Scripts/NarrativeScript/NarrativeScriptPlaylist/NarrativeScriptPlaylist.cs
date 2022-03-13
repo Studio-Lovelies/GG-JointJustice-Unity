2e8e34c745960f21582d266a0ccd59c249241509
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-03-13 19:00:32 +0000

    Removed unused argument

--- unity-ggjj/Assets/Scripts/NarrativeScript/NarrativeScriptPlaylist/NarrativeScriptPlaylist.cs
+++ unity-ggjj/Assets/Scripts/NarrativeScript/NarrativeScriptPlaylist/NarrativeScriptPlaylist.cs
@@ -17,8 +17,7 @@ public class NarrativeScriptPlaylist : MonoBehaviour, INarrativeScriptPlaylist
     /// <summary>
     /// Call the initialise method on all narrative scripts in this playlist
     /// </summary>
-    /// <param name="bgSceneList">A BGSceneList object with which to instantiate BGScenes used by narrative scripts</param>
-    public void InitializeNarrativeScripts(BGSceneList bgSceneList)
+    public void InitializeNarrativeScripts()
     {
         if (DefaultNarrativeScript.Script == null) { return; }
         
