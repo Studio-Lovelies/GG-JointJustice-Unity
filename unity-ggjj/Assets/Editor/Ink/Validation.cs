c458d2707d46d5627f1110eeedf12460ea7cc6b0
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-03-22 02:51:03 +0000

    Implemented vimaster's better code

--- unity-ggjj/Assets/Editor/Ink/Validation.cs
+++ unity-ggjj/Assets/Editor/Ink/Validation.cs
@@ -13,16 +13,9 @@ namespace Editor.Ink
         /// <summary>
         /// Called when an Ink file is compiled after editing it
         /// </summary>
-        private static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
-            foreach (var importedAssetPath in importedAssets)
-            {
-                if (!InkEditorUtils.IsInkFile(importedAssetPath))
-                {
-                    continue;
-                }
-                var inkFile = InkLibrary.GetInkFileWithPath(importedAssetPath);
-                ValidateNarrativeScript(inkFile);
-            }
+        private static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            FindAndReportErrorsForFiles(importedAssets.Where(InkEditorUtils.IsInkFile).Select(InkLibrary.GetInkFileWithPath));
         }
 
         /// <summary>
@@ -31,9 +24,20 @@ namespace Editor.Ink
         [UnityEditor.MenuItem("Assets/Validate Narrative Scripts", false, 202)]
         private static void ValidateAllNarrativeScripts()
         {
-            var noErrors = InkLibrary.GetMasterInkFiles().All(inkFile => !ValidateNarrativeScript(inkFile));
+            FindAndReportErrorsForFiles(InkLibrary.GetMasterInkFiles());
+        }
+        
+        /// <summary>
+        /// Uses <see cref="FindErrorsInFile"/> to generate errors for all <see cref="inkFiles"/> and print them to the console
+        /// Prints a log message if the process completed without any errors
+        /// </summary>
+        /// <param name="inkFiles"><see cref="InkFile"/> instances to check lines for errors</param>
+        private static void FindAndReportErrorsForFiles(IEnumerable<InkFile> inkFiles)
+        {
+            var errorsInFiles = inkFiles.Select(FindErrorsInFile).SelectMany(error => error).ToList();
+            errorsInFiles.ForEach(Debug.Log);
 
-            if (noErrors)
+            if (!errorsInFiles.Any())
             {
                 Debug.Log("Narrative Scripts validated without errors");
             }
@@ -41,16 +45,16 @@ namespace Editor.Ink
 
         /// <summary>
         /// Reads an Ink file and attempts to parse every action.
-        /// Logs any errors found to the console
+        /// Returns a collection of all errors found
         /// </summary>
         /// <param name="inkFile">The Ink file to read</param>
-        private static bool ValidateNarrativeScript(InkFile inkFile)
+        private static IEnumerable<string> FindErrorsInFile(InkFile inkFile)
         {
+            var errors = new List<string>();
+            
             var lines = new List<string>();
             var story = new Story(inkFile.jsonAsset.text);
             NarrativeScript.ReadContent(story.mainContentContainer.content, lines, story);
-
-            var noErrors = true;
 
             foreach (var line in lines.Where(t => t[0] == ActionDecoderBase.ACTION_TOKEN))
             {
@@ -60,12 +64,11 @@ namespace Editor.Ink
                 }
                 catch (Exception exception)
                 {
-                    noErrors = false;
-                    Debug.LogError($"Error in {inkFile} on line \"{line}\"\n{exception.Message}");
+                    errors.Add($"Error in {inkFile} on line \"{line}\"\n{exception.Message}");
                 }
             }
 
-            return noErrors;
+            return errors;
         }
     }
 }
