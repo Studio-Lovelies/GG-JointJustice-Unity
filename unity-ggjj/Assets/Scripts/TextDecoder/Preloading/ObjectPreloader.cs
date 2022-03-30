d1a815ee3c02608d29191b2b696a177c16ef8b23
moths <67964179+PhoebeMitchell@users.noreply.github.com>
2022-03-31 00:10:35 +0100

    Update unity-ggjj/Assets/Scripts/TextDecoder/Preloading/ObjectPreloader.cs
    Co-authored-by: Vincent Mahnke <vincent@mahn.ke>

--- unity-ggjj/Assets/Scripts/TextDecoder/Preloading/ObjectPreloader.cs
+++ unity-ggjj/Assets/Scripts/TextDecoder/Preloading/ObjectPreloader.cs
@@ -99,7 +99,7 @@ public class ObjectPreloader : ActionDecoderBase
         }
         catch (NullReferenceException exception)
         {
-            throw new NullReferenceException($"{exception.GetType().Name}: Object at path {path} could not be loaded");
+            throw new NullReferenceException($"{exception.GetType().Name}: Object at path {path} could not be loaded", exception);
         }
     }
 }
