fe05b292e027927d92f04a3eb80b0b6ad39e5349
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-06-26 21:09:14 +0100

    Fixed being able to have duplicate bindings

--- unity-ggjj/Assets/Scripts/UI/ControlButton.cs
+++ unity-ggjj/Assets/Scripts/UI/ControlButton.cs
@@ -9,6 +9,8 @@ namespace UI
         [SerializeField] private InputActionReference _inputActionReference;
         [SerializeField] private TextMeshProUGUI _text;
 
+        private InputActionRebindingExtensions.RebindingOperation _rebindingOperation;
+        
         private void Awake()
         {
             UpdateButton();
@@ -17,9 +19,11 @@ namespace UI
         public void BeginRebind()
         {
             _inputActionReference.action.Disable();
-            _inputActionReference.action.PerformInteractiveRebinding(0)
-                .WithControlsExcluding("Mouse")
+            _rebindingOperation = _inputActionReference.action.PerformInteractiveRebinding(0)
+                .WithControlsExcluding("<Mouse>")
+                .OnPotentialMatch(OnPotentialMatch)
                 .OnComplete(OnRebindComplete)
+                .OnCancel(OnRebindComplete)
                 .OnMatchWaitForAnother(0.1f)
                 .Start();
         }
@@ -28,12 +32,32 @@ namespace UI
         {
             _inputActionReference.action.Enable();
             rebindingOperation.Dispose();
+            _rebindingOperation = null;
             UpdateButton();
+        }
+
+        private void OnPotentialMatch(InputActionRebindingExtensions.RebindingOperation rebindingOperation)
+        {
+            foreach (var action in _inputActionReference.asset.actionMaps[0].actions)
+            {
+                foreach (var control in action.controls)
+                {
+                    if (control == rebindingOperation.selectedControl)
+                    {
+                        rebindingOperation.Cancel();
+                    }
+                }
+            }
         }
 
         private void UpdateButton()
         {
             _text.text = _inputActionReference.action.GetBindingDisplayString().Split('|')[0].Trim();
+        }
+
+        public void Cancel()
+        {
+            _rebindingOperation?.Cancel();
         }
     }
 }
