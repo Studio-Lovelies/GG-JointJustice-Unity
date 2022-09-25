ab9f1666dbdf74083370fe012a22362f2f13c35e
Phoebe Mitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2022-09-25 18:57:30 +0100

    Fixed tests

--- unity-ggjj/Assets/Tests/PlayModeTests/Scenes/MainMenu/ViaMouse.cs
+++ unity-ggjj/Assets/Tests/PlayModeTests/Scenes/MainMenu/ViaMouse.cs
@@ -13,6 +13,7 @@ namespace Tests.PlayModeTests.Scenes.MainMenu
     {
         private readonly InputTestTools _inputTestTools = new InputTestTools();
         private Mouse Mouse => _inputTestTools.mouse;
+        private Keyboard Keyboard => _inputTestTools.keyboard;
 
         [SetUp]
         public void Setup()
@@ -39,13 +40,11 @@ namespace Tests.PlayModeTests.Scenes.MainMenu
             // and we query all existing menus instead
             var menus = TestTools.FindInactiveInScene<Menu>();
             var mainMenu = menus.First(menu => menu.gameObject.name == "MenuButtons");
-            var subMenu = menus.First(menu => menu.gameObject.name == "TestSubMenu");
-            var secondSubMenu = menus.First(menu => menu.gameObject.name == "TestDoubleSubMenu");
+            var subMenu = menus.First(menu => menu.gameObject.name == "SettingsMenu");
+            var secondSubMenu = menus.First(menu => menu.gameObject.name == "ControlsMenu");
             
             var openFirstSubMenuButton = mainMenu.gameObject.GetComponentsInChildren<RectTransform>().First(menuItem => menuItem.gameObject.name == "SettingsButton");
-            var openSecondSubMenuButton = subMenu.gameObject.GetComponentsInChildren<RectTransform>().First(menuItem => menuItem.gameObject.name == "LoadButton (1)");
-            var closeSecondSubMenuButton = secondSubMenu.gameObject.GetComponentsInChildren<RectTransform>().First(menuItem => menuItem.gameObject.name == "LoadButton (4)");
-            var closeFirstSubMenuButton = subMenu.gameObject.GetComponentsInChildren<RectTransform>().First(menuItem => menuItem.gameObject.name == "LoadButton (4)");
+            var openSecondSubMenuButton = subMenu.gameObject.GetComponentsInChildren<RectTransform>().First(menuItem => menuItem.gameObject.name == "ControlsMenuButton");
 
             Assert.True(mainMenu.Active);
             Assert.False(subMenu.Active);
@@ -62,53 +61,15 @@ namespace Tests.PlayModeTests.Scenes.MainMenu
             Assert.True(secondSubMenu.Active);
             Assert.False(subMenu.Active);
 
-            yield return _inputTestTools.SetMouseScreenSpacePosition(closeSecondSubMenuButton.position + closeSecondSubMenuButton.localScale * 0.5f);
-            yield return _inputTestTools.PressForFrame(Mouse.leftButton);
+            yield return _inputTestTools.PressForFrame(Keyboard.escapeKey);
 
             Assert.True(subMenu.Active);
             Assert.False(mainMenu.Active);
 
-            yield return _inputTestTools.SetMouseScreenSpacePosition(closeFirstSubMenuButton.position + closeFirstSubMenuButton.localScale * 0.5f);
-            yield return _inputTestTools.PressForFrame(Mouse.leftButton);
+            yield return _inputTestTools.PressForFrame(Keyboard.escapeKey);
 
             Assert.True(mainMenu.Active);
             Assert.False(subMenu.Active);
-        }
-
-        [UnityTest]
-        public IEnumerator CanEnterAndCloseTwoSubMenusWithCloseAllButton()
-        {
-            // as the containing GameObjects are enabled, `GameObject.Find()` will not find them
-            // and we query all existing menus instead
-            var menus = TestTools.FindInactiveInScene<Menu>();
-            var mainMenu = menus.First(menu => menu.gameObject.name == "MenuButtons");
-            var subMenu = menus.First(menu => menu.gameObject.name == "TestSubMenu");
-            var secondSubMenu = menus.First(menu => menu.gameObject.name == "TestDoubleSubMenu");
-            
-            var openFirstSubMenuButton = mainMenu.gameObject.GetComponentsInChildren<RectTransform>().First(menuItem => menuItem.gameObject.name == "SettingsButton");
-            var openSecondSubMenuButton = subMenu.gameObject.GetComponentsInChildren<RectTransform>().First(menuItem => menuItem.gameObject.name == "LoadButton (1)");
-            var closeAllSubMenusButton = secondSubMenu.gameObject.GetComponentsInChildren<RectTransform>().First(menuItem => menuItem.gameObject.name == "LoadButton (1)");
-
-            Assert.True(mainMenu.Active);
-            Assert.False(subMenu.Active);
-
-            yield return _inputTestTools.SetMouseScreenSpacePosition(openFirstSubMenuButton.position + openFirstSubMenuButton.localScale * 0.5f);
-            yield return _inputTestTools.PressForFrame(Mouse.leftButton);
-
-            Assert.True(subMenu.Active);
-            Assert.False(mainMenu.Active);
-
-            yield return _inputTestTools.SetMouseScreenSpacePosition(openSecondSubMenuButton.position + openSecondSubMenuButton.localScale * 0.5f);
-            yield return _inputTestTools.PressForFrame(Mouse.leftButton);
-
-            Assert.True(secondSubMenu.Active);
-            Assert.False(subMenu.Active);
-
-            yield return _inputTestTools.SetMouseScreenSpacePosition(closeAllSubMenusButton.position + closeAllSubMenusButton.localScale * 0.5f);
-            yield return _inputTestTools.PressForFrame(Mouse.leftButton);
-
-            Assert.True(mainMenu.Active);
-            Assert.False(secondSubMenu.Active);
         }
 
         [UnityTest]
