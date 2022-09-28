d08ac5da4dd9b9f53fcca4c8652ffc1222eff62b
Vincent <vincent@mahn.ke>
2022-09-29 00:44:17 +0200

    chore: Split 'FadeBetweenSongs' test into Static and Dynamic

--- unity-ggjj/Assets/Tests/PlayModeTests/Scripts/AudioController/AudioControllerTests.cs
+++ unity-ggjj/Assets/Tests/PlayModeTests/Scripts/AudioController/AudioControllerTests.cs
@@ -13,68 +13,129 @@ namespace Tests.PlayModeTests.Scripts.AudioController
     //       To test this locally, activate `Edit` -> `Project Settings` -> `Audio` -> `Disable Unity Audio`.
     public class AudioControllerTests
     {
-        private const string MUSIC_PATH = "Audio/Music/Static/";
+        private const string MUSIC_PATH = "Audio/Music/";
         
         private global::AudioController _audioController;
-        private AudioSource _audioSource;
-        private VolumeManager _volumeManager;
+        private AudioSource _musicPrimaryAudioSource;
+        private VolumeManager _musicPrimaryVolumeManager;
+
+        private AudioSource _musicSecondaryAAudioSource;
+        private VolumeManager _musicSecondaryAVolumeManager;
         
         [UnitySetUp]
         public IEnumerator SetUp()
         {
             yield return SceneManager.LoadSceneAsync("Game");
-            var audioSourceGameObject = GameObject.Find("MusicPrimary");
+            
+            var musicPrimaryGameObject = GameObject.Find("MusicPrimary");
+            _musicPrimaryAudioSource = musicPrimaryGameObject.GetComponent<AudioSource>();
+            _musicPrimaryVolumeManager = musicPrimaryGameObject.GetComponent<VolumeManager>();
+            
+            var musicSecondaryAGameObject = GameObject.Find("MusicSecondaryA");
+            _musicSecondaryAAudioSource = musicSecondaryAGameObject.GetComponent<AudioSource>();
+            _musicSecondaryAVolumeManager = musicSecondaryAGameObject.GetComponent<VolumeManager>();
+
             _audioController = Object.FindObjectOfType<global::AudioController>();
-            _audioSource = audioSourceGameObject.GetComponent<AudioSource>();
-            _volumeManager = audioSourceGameObject.GetComponent<VolumeManager>();
         }
         
         [UnityTest]
-        public IEnumerator FadesBetweenSongs()
+        public IEnumerator FadesBetweenStaticSongs()
         {
             const float TRANSITION_DURATION = 2f;
 
             // setup and verify steady state of music playing for a while
-            var firstSong = LoadSong("aBoyAndHisTrial");
+            var firstSong = LoadStaticSong("aBoyAndHisTrial");
+            Assert.IsNotNull(firstSong);
             _audioController.PlayStaticSong(firstSong, TRANSITION_DURATION);
 
-            yield return TestTools.WaitForState(() => _audioSource.volume == _volumeManager.MaximumVolume, TRANSITION_DURATION*2);
-            Assert.AreEqual(firstSong.name, _audioSource.clip.name);
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume == _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION*2);
+            Assert.AreEqual(firstSong.name, _musicPrimaryAudioSource.clip.name);
 
             // transition into new song
-            var secondSong = LoadSong("aKissFromARose");
+            var secondSong = LoadStaticSong("aKissFromARose");
+            Assert.IsNotNull(secondSong);
             _audioController.PlayStaticSong(secondSong, TRANSITION_DURATION);
-            yield return TestTools.WaitForState(() => _audioSource.volume != _volumeManager.MaximumVolume, TRANSITION_DURATION);
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume != _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION);
 
             // expect old song to still be playing, but no longer at full volume, as we're transitioning
-            Assert.AreNotEqual(_audioSource.volume, _volumeManager.MaximumVolume);
-            Assert.AreEqual(firstSong.name, _audioSource.clip.name);
+            Assert.AreNotEqual(_musicPrimaryAudioSource.volume, _musicPrimaryVolumeManager.MaximumVolume);
+            Assert.AreEqual(firstSong.name, _musicPrimaryAudioSource.clip.name);
 
             // expect new song to be playing at full volume, as we're done transitioning
-            yield return TestTools.WaitForState(() => _audioSource.volume == _volumeManager.MaximumVolume, TRANSITION_DURATION*2);
-            Assert.AreEqual(secondSong.name, _audioSource.clip.name);
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume == _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION*2);
+            Assert.AreEqual(secondSong.name, _musicPrimaryAudioSource.clip.name);
 
             // transition into new song
-            var thirdSong = LoadSong("investigationJoonyer");
+            var thirdSong = LoadStaticSong("investigationJoonyer");
+            Assert.IsNotNull(thirdSong);
             _audioController.PlayStaticSong(thirdSong, TRANSITION_DURATION);
 
             // expect old song to still be playing, but no longer at full volume, as we're transitioning
-            yield return TestTools.WaitForState(() => _audioSource.volume != _volumeManager.MaximumVolume, TRANSITION_DURATION);
-            Assert.AreEqual(secondSong.name, _audioSource.clip.name);
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume != _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION);
+            Assert.AreEqual(secondSong.name, _musicPrimaryAudioSource.clip.name);
 
             // expect new song to be playing at full volume, as we're done transitioning
-            yield return TestTools.WaitForState(() => _audioSource.volume == _volumeManager.MaximumVolume, TRANSITION_DURATION*2);
-            Assert.AreEqual(thirdSong.name, _audioSource.clip.name);
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume == _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION*2);
+            Assert.AreEqual(thirdSong.name, _musicPrimaryAudioSource.clip.name);
+        }
+        
+        [UnityTest]
+        public IEnumerator FadesBetweenDynamicSongs()
+        {
+            const float TRANSITION_DURATION = 2f;
+
+            const string FIRST_SONG_NAME = "YouBurgieBurgie";
+            const string FIRST_SONG_VARIANT_NAME = "Dan";
+            const string SECOND_SONG_NAME = "YouBurgieBurgie_2";
+            const string SECOND_SONG_VARIANT_NAME = "Dan";
+
+            // setup and verify steady state of music playing for a while
+            var firstSong = LoadDynamicSong(FIRST_SONG_NAME);
+            Assert.IsNotNull(firstSong);
+            _audioController.PlayDynamicSong(firstSong, FIRST_SONG_VARIANT_NAME, TRANSITION_DURATION);
+
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume == _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION*2);
+            Assert.AreEqual(firstSong.Primary.name, _musicPrimaryAudioSource.clip.name);
+            Assert.AreEqual($"{FIRST_SONG_NAME}-{FIRST_SONG_VARIANT_NAME}", _musicSecondaryAAudioSource.clip.name);
+
+            // transition into new song
+            var secondSong = LoadDynamicSong(SECOND_SONG_NAME);
+            Assert.IsNotNull(secondSong);
+            _audioController.PlayDynamicSong(secondSong, SECOND_SONG_VARIANT_NAME, TRANSITION_DURATION);
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume != _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION);
+
+            // expect old song to still be playing, but no longer at full volume, as we're transitioning
+            Assert.AreNotEqual(_musicPrimaryAudioSource.volume, _musicPrimaryVolumeManager.MaximumVolume);
+            Assert.AreEqual(firstSong.Primary.name, _musicPrimaryAudioSource.clip.name);
+            Assert.AreEqual($"{FIRST_SONG_NAME}-{FIRST_SONG_VARIANT_NAME}", _musicSecondaryAAudioSource.clip.name);
+
+            // expect new song to be playing at full volume, as we're done transitioning
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume == _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION*2);
+            Assert.AreEqual(secondSong.Primary.name, _musicPrimaryAudioSource.clip.name);
+            Assert.AreEqual($"{SECOND_SONG_NAME}-{SECOND_SONG_VARIANT_NAME}", _musicSecondaryAAudioSource.clip.name);
+
+            // transition back into first song
+            _audioController.PlayDynamicSong(firstSong, FIRST_SONG_VARIANT_NAME, TRANSITION_DURATION);
+
+            // expect old song to still be playing, but no longer at full volume, as we're transitioning
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume != _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION);
+            Assert.AreEqual(secondSong.Primary.name, _musicPrimaryAudioSource.clip.name);
+            Assert.AreEqual($"{SECOND_SONG_NAME}-{SECOND_SONG_VARIANT_NAME}", _musicSecondaryAAudioSource.clip.name);
+
+            // expect new song to be playing at full volume, as we're done transitioning
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume == _musicPrimaryVolumeManager.MaximumVolume, TRANSITION_DURATION*2);
+            Assert.AreEqual(firstSong.Primary.name, _musicPrimaryAudioSource.clip.name);
+            Assert.AreEqual($"{FIRST_SONG_NAME}-{FIRST_SONG_VARIANT_NAME}", _musicSecondaryAAudioSource.clip.name);
         }
 
         [UnityTest]
         public IEnumerator SongsCanBePlayedWithoutFadeIn()
         {
-            var song = LoadSong("aBoyAndHisTrial");
+            var song = LoadStaticSong("aBoyAndHisTrial");
             _audioController.PlayStaticSong(song, 0);
             yield return null;
-            Assert.AreEqual(song.name, _audioSource.clip.name);
-            Assert.AreEqual(_volumeManager.MaximumVolume, _audioSource.volume);
+            Assert.AreEqual(song.name, _musicPrimaryAudioSource.clip.name);
+            Assert.AreEqual(_musicPrimaryVolumeManager.MaximumVolume, _musicPrimaryAudioSource.volume);
         }
 
         [UnityTest]
@@ -84,16 +145,21 @@ namespace Tests.PlayModeTests.Scripts.AudioController
             yield return SongsCanBePlayedWithoutFadeIn();
             var expectedEndTimeOfFadeOut = Time.time + TRANSITION_DURATION_IN_SECONDS;
             _audioController.FadeOutSong(TRANSITION_DURATION_IN_SECONDS);
-            yield return TestTools.WaitForState(() => _audioSource.volume == 0);
+            yield return TestTools.WaitForState(() => _musicPrimaryAudioSource.volume == 0);
 
             // This should take at least TRANSITION_DURATION_IN_SECONDS
             Assert.GreaterOrEqual(Time.time, expectedEndTimeOfFadeOut);
-            Assert.AreEqual(0, _audioSource.volume);
+            Assert.AreEqual(0, _musicPrimaryAudioSource.volume);
         }
 
-        public static AudioClip LoadSong(string songName)
+        public static AudioClip LoadStaticSong(string songName)
         {
-            return Resources.Load<AudioClip>($"{MUSIC_PATH}{songName}");
+            return Resources.Load<AudioClip>($"{MUSIC_PATH}Static/{songName}");
+        }
+
+        public static DynamicMusicData LoadDynamicSong(string songName)
+        {
+            return Resources.Load<DynamicMusicData>($"{MUSIC_PATH}Dynamic/{songName}/{songName}");
         }
     }
 }
