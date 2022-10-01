2d74da6e2c950cff66d4e52cbe25d4717a9725a2
Vincent Mahnke <vincent@mahn.ke>
2022-10-01 23:06:20 +0200

    test: Increase coverage for TextDecoder/Parser.cs

---
+++ unity-ggjj/Assets/Tests/EditModeTests/TextDecoder/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TextDecoder.Parser;
+
+namespace Tests.EditModeTests.TextDecoder
+{
+    public class Parser
+    {
+        private static IEnumerable<Type> AllAssetNameParser => typeof(Parser<>).Assembly.GetTypes().Where(type => type.BaseType is { IsGenericType: true } && type.BaseType.GetGenericTypeDefinition() == typeof(Parser<>));
+
+        [Test]
+        [TestCaseSource(nameof(AllAssetNameParser))]
+        public void ThrowsIfStringIsEmpty(Type parser)
+        {
+            var parserConstructor = parser.GetConstructor(Type.EmptyTypes);
+            var parserInstance = parserConstructor.Invoke(Array.Empty<object>());
+            object[] parseMethodParameters = { "", null };
+            string result = (string)parser.GetMethod("Parse")!.Invoke(parserInstance, parseMethodParameters);
+            Assert.Greater(result.Length, 0);
+        }
+
+    }
+}
