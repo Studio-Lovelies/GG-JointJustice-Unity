c1981bcb70eef25fd228b80131b4a78c6b3d4749
Vincent Mahnke <vincent@mahn.ke>
2023-03-18 22:54:23 +0100

    feat(NarrativeScriptWatcher): Added tests and example script

--- unity-ggjj/Assets/Scripts/RuntimeEditing/NarrativeScriptWatcher.cs
+++ unity-ggjj/Assets/Scripts/RuntimeEditing/NarrativeScriptWatcher.cs
@@ -1,5 +1,8 @@
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TestTools;
 
 namespace RuntimeEditing
 {
@@ -8,11 +11,41 @@ namespace RuntimeEditing
     {
         [SerializeField]
         private NarrativeGameState narrativeGameState;
+        public string AbsolutePathToWatchedScript { get; private set; }
+
         private DebugControls _debugControls;
-        private string _absolutePathToScript;
         private string _absolutePathToCompiledScript;
         private FileSystemWatcher _fileSystemWatcher;
         private bool _fileHasChanged;
+        
+        private readonly string _exampleScript = @"
+                &SCENE:TMPH_Court
+                &SET_ACTOR_POSITION:Defense,Arin
+                &SET_ACTOR_POSITION:Witness,Ross
+
+                &JUMP_TO_POSITION:Defense
+                &SPEAK:Arin
+                Okay, so you claim to have seen my client at the scene of the crime, and you base your entire testimony on a game of Mario Party?
+
+                &JUMP_TO_POSITION:Witness
+                &SPEAK:Ross
+                Yes, that's correct.
+
+                &JUMP_TO_POSITION:Defense
+                &SPEAK:Arin
+                And yet, you're the same person who made those incredibly difficult Mario Maker levels that almost broke me?
+
+                &JUMP_TO_POSITION:Witness
+                &SPEAK:Ross
+                Uh, yeah? Also what does that have to do with anything?
+
+                &JUMP_TO_POSITION:Defense
+                &SPEAK:Arin
+                Well, maybe look at what the writers of this script came up with!
+                ...
+                Absolutely nothing.
+                Good.
+                I am glad.".Replace("  ", "").Trim();
 
         private void Awake()
         {
@@ -23,11 +56,10 @@ namespace RuntimeEditing
             }
         
             const string RELATIVE_SCRIPT_PATH = "/../DevKit/Script.ink";
-            _absolutePathToScript = Path.GetFullPath(Application.dataPath + RELATIVE_SCRIPT_PATH);
+            AbsolutePathToWatchedScript = Path.GetFullPath(Application.dataPath + RELATIVE_SCRIPT_PATH);
             const string RELATIVE_COMPILATION_TARGET_PATH = "/../Temp/Script.ink.json";
             _absolutePathToCompiledScript = Path.GetFullPath(Application.dataPath + RELATIVE_COMPILATION_TARGET_PATH);
         
-            narrativeGameState = GetComponent<NarrativeGameState>();
             _debugControls = new DebugControls();
             _debugControls.Enable();
             _debugControls.KeyboardMouse.ReloadScript.performed += _ =>
@@ -36,22 +68,18 @@ namespace RuntimeEditing
                 ReloadGame();
             };
 
-            _debugControls.KeyboardMouse.OpenEditor.performed += _ =>
-            {
-                Debug.Log($"{nameof(NarrativeScriptWatcher)}: Opening '{_absolutePathToScript}'");
-                var process = new System.Diagnostics.Process();
-                process.StartInfo.FileName = _absolutePathToScript;
-                process.Start();
-            };
+            _debugControls.KeyboardMouse.OpenEditor.performed += OpenTextEditorForScript;
         
-            if (!File.Exists(_absolutePathToScript))
+            if (!File.Exists(AbsolutePathToWatchedScript))
             {
-                File.Create(_absolutePathToScript);
+                using var stream = File.Open(AbsolutePathToWatchedScript, FileMode.Create, FileAccess.Write);
+                using var writer = new StreamWriter(stream);
+                writer.Write(_exampleScript);
             }
         
             _fileSystemWatcher = new FileSystemWatcher();
-            _fileSystemWatcher.Path = Directory.GetParent(_absolutePathToScript)!.FullName;
-            _fileSystemWatcher.Filter = Path.GetFileName(_absolutePathToScript);
+            _fileSystemWatcher.Path = Directory.GetParent(AbsolutePathToWatchedScript)!.FullName;
+            _fileSystemWatcher.Filter = Path.GetFileName(AbsolutePathToWatchedScript);
             _fileSystemWatcher.IncludeSubdirectories = false;
             _fileSystemWatcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.Size | NotifyFilters.LastWrite;
             _fileSystemWatcher.Changed += (sender, args) =>
@@ -59,7 +87,23 @@ namespace RuntimeEditing
                 _fileHasChanged = true;
             };
             _fileSystemWatcher.EnableRaisingEvents = true;
-            Debug.Log($"{nameof(NarrativeScriptWatcher)}: Watching '{_absolutePathToScript}'...");
+            Debug.Log($"{nameof(NarrativeScriptWatcher)}: Watching '{AbsolutePathToWatchedScript}'...");
+        }
+
+        [ExcludeFromCoverage]
+        private void OpenTextEditorForScript(InputAction.CallbackContext _)
+        {
+            Debug.Log($"{nameof(NarrativeScriptWatcher)}: Opening '{AbsolutePathToWatchedScript}'");
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = AbsolutePathToWatchedScript;
+            process.Start();
+        }
+
+        private void OnDestroy()
+        {
+            _debugControls?.Dispose();
+            _fileSystemWatcher.EnableRaisingEvents = false;
+            _fileSystemWatcher?.Dispose();
         }
 
         private void Update()
@@ -70,7 +114,7 @@ namespace RuntimeEditing
             }
 
             _fileHasChanged = false;
-            Debug.Log($"{nameof(NarrativeScriptWatcher)}: Change detected: '{_absolutePathToScript}'");  
+            Debug.Log($"{nameof(NarrativeScriptWatcher)}: Change detected: '{AbsolutePathToWatchedScript}'");  
             ReloadGame();
         }
 
@@ -82,9 +126,25 @@ namespace RuntimeEditing
             }
 
             Debug.Log($"{nameof(NarrativeScriptWatcher)}: Compiling debug script to '{_absolutePathToCompiledScript}'");
-            var compiler = new Ink.Compiler(File.ReadAllText(_absolutePathToScript));
-            var output = compiler.Compile().ToJson();
-            File.WriteAllText(_absolutePathToCompiledScript, output);
+            Ink.Compiler compiler;
+            _fileSystemWatcher.EnableRaisingEvents = false;
+            using (var stream = File.Open(AbsolutePathToWatchedScript, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    compiler = new Ink.Compiler(reader.ReadToEnd());
+                    compiler.Compile();
+                }
+            }
+            _fileSystemWatcher.EnableRaisingEvents = true;
+
+            using (var stream = File.Open(_absolutePathToCompiledScript, FileMode.Create, FileAccess.Write))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write(compiler.Compile().ToJson());
+                }
+            }
 
             Debug.Log($"{nameof(NarrativeScriptWatcher)}: Restarting game...");
             var narrativeScript = new NarrativeScript(new TextAsset(File.ReadAllText(_absolutePathToCompiledScript)));
