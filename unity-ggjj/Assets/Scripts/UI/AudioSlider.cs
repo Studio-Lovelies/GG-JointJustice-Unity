b9ea7e6d57bb148e578a79727f98ff5e9fdb9dd3
PhoebeMitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2023-06-03 00:38:23 +0100

    Added comments

--- unity-ggjj/Assets/Scripts/UI/AudioSlider.cs
+++ unity-ggjj/Assets/Scripts/UI/AudioSlider.cs
@@ -40,27 +40,47 @@ public class AudioSlider : MonoBehaviour
         _slider.onValueChanged.AddListener(SetValue);
     }
 
+    /// <summary>
+    /// Updates the value of the slider UI component to be the same as the mixer group value
+    /// </summary>
     public void UpdateSliderValue()
     {
         _audioMixerGroup.audioMixer.GetFloat(VolumeParameterName, out var value);
         _slider.value = CalculateLinearVolume(value);
     }
 
+    /// <summary>
+    /// Method used by the UI slider's OnValueChanged event to update the volume of the audio mixer
+    /// </summary>
+    /// <param name="value">The value to set the slider to. Takes a value from 0 to 1</param>
     private void SetValue(float value)
     {
         _audioMixerGroup.audioMixer.SetFloat(VolumeParameterName, CalculateVolumeInDecibels(value));
     }
 
+    /// <summary>
+    /// Boilerplate function to allow selecting of the UI slider
+    /// </summary>
     public void Select()
     {
         _slider.Select();
     }
 
+    /// <summary>
+    /// Converts a value from -80 to 0 to a value from 0 to 1
+    /// I.E converts from decibels used by AudioMixer to a linear value
+    /// </summary>
+    /// <param name="volumeInDecibels">The volume in decibels to convert. Takes a value from -80 to 0</param>
     private static float CalculateLinearVolume(float volumeInDecibels)
     {
         return Mathf.Pow(10, volumeInDecibels / MAX_DECIBELS);
     }
 
+    /// <summary>
+    /// Converts a value from 0 to 1 to a value from -80 to 0
+    /// I.E converts from a linear value to decibels used by AudioMixer
+    /// </summary>
+    /// <param name="linearVolume">The value to convert to decibels. Takes a value from 0 to 1</param>
     private static float CalculateVolumeInDecibels(float linearVolume)
     {
         if (linearVolume == 0)
@@ -70,11 +90,20 @@ public class AudioSlider : MonoBehaviour
         return MAX_DECIBELS * Mathf.Log(linearVolume, DECIBEL_LOG_BASE);
     }
 
+    /// <summary>
+    /// Activates the sound played when changing the value of the slider
+    /// This allows the value to be changed before activation without playing the sound
+    /// Used when instantiating sliders and setting their initial values
+    /// </summary>
     public void ActivateOnValueChangedSound()
     {
         _slider.onValueChanged.AddListener(_ => PlayUpdateSound());
     }
 
+    /// <summary>
+    /// Method used by the onValueChanged event of the UI slider to play a sound
+    /// Only allows a sound to be played after a certain period of time to prevent audio spam
+    /// </summary>
     private void PlayUpdateSound()
     {
         var unscaledTime = Time.unscaledTime;
