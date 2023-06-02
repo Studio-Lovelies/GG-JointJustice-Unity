ed11c9817f535ba2be81436c0ce760400ea11191
PhoebeMitchell <67964179+PhoebeMitchell@users.noreply.github.com>
2023-06-02 23:43:28 +0100

    Fixed scaling of audio

--- unity-ggjj/Assets/Scripts/UI/AudioSlider.cs
+++ unity-ggjj/Assets/Scripts/UI/AudioSlider.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class AudioSlider : MonoBehaviour
 {
-    private const int MIN_DECIBALS = -80;
-    private const int MAX_DECIBALS = 20;
+    private const int MIN_DECIBELS = -80;
+    private const int MAX_DECIBELS = 20;
+    private const int DECIBEL_LOG_BASE = 10;
     
     private TextMeshProUGUI _label;
     private AudioMixerGroup _audioMixerGroup;
@@ -20,7 +21,7 @@ public class AudioSlider : MonoBehaviour
         set
         {
             _audioMixerGroup = value;
-            UpdateValue();
+            UpdateSliderValue();
         }
     }
     
@@ -37,19 +38,33 @@ public class AudioSlider : MonoBehaviour
         _slider.onValueChanged.AddListener(SetValue);
     }
 
-    private void UpdateValue()
+    private void UpdateSliderValue()
     {
         _audioMixerGroup.audioMixer.GetFloat(VolumeParameterName, out var value);
-        _slider.value = Mathf.InverseLerp(MIN_DECIBALS, MAX_DECIBALS, value);
+        _slider.value = Mathf.InverseLerp(MIN_DECIBELS, MAX_DECIBELS, CalculateLinearVolume(value));
     }
 
     private void SetValue(float value)
     {
-        _audioMixerGroup.audioMixer.SetFloat(VolumeParameterName, Mathf.Lerp(MIN_DECIBALS, MAX_DECIBALS, value));
+        _audioMixerGroup.audioMixer.SetFloat(VolumeParameterName, CalculateVolumeInDecibels(value));
     }
 
     public void Select()
     {
         _slider.Select();
+    }
+
+    private static float CalculateLinearVolume(float volumeInDecibels)
+    {
+        return Mathf.Pow(volumeInDecibels / MAX_DECIBELS, DECIBEL_LOG_BASE);
+    }
+
+    private static float CalculateVolumeInDecibels(float linearVolume)
+    {
+        if (linearVolume == 0)
+        {
+            return MIN_DECIBELS;
+        }
+        return MAX_DECIBELS * Mathf.Log(linearVolume, DECIBEL_LOG_BASE);
     }
 }
