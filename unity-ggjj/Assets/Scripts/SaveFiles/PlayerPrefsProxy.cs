57731e3039b3c5b7077a2b9453985197457ed255
moths <67964179+PhoebeMitchell@users.noreply.github.com>
2023-07-05 13:09:49 +0100

    Updated to 2022.3.4f1 LTS

--- unity-ggjj/Assets/Scripts/SaveFiles/PlayerPrefsProxy.cs
+++ unity-ggjj/Assets/Scripts/SaveFiles/PlayerPrefsProxy.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using Unity.Plastic.Newtonsoft.Json;
+using Unity.Plastic.Newtonsoft.Json.Linq;
 using UnityEngine;
 
 namespace SaveFiles
