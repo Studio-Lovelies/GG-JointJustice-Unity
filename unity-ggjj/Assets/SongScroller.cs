7354fb0535a4bae9b9cf55084f2b578b9824659b
Vincent Mahnke <vincent@mahn.ke>
2023-08-19 14:48:01 +0200

    refactor: Makes SongScroller "more pixelated"

--- unity-ggjj/Assets/SongScroller.cs
+++ unity-ggjj/Assets/SongScroller.cs
@@ -1,35 +1,63 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SongScroller : MonoBehaviour
 {
+    private const int CharacterMaximum = 27;
     [SerializeField]
     private ScrollRect _scrollRect;
+    [SerializeField]
+    private TextMeshProUGUI _textMeshProUGUI;
 
     public Animator _animator;
+    private string _fullTrackName;
 
+    public void ShowTrack(string fullTrackName)
+    {
+        _fullTrackName = fullTrackName;
+        _textMeshProUGUI.text = GenerateSubstringString(0);
+    }
+    
     public void StartScrolling()
     {
         StartCoroutine(Scroll());
     }
 
-    // scroll to the right for 5 seconds and lerp with ease-in-out
+    private string GenerateSubstringString(int offset)
+    {
+        var content = _fullTrackName.Substring(offset, Math.Min(CharacterMaximum, _fullTrackName.Length - offset));
+        content = content.PadRight(CharacterMaximum, ' ');
+        return content;
+    }
+    
     private IEnumerator Scroll()
     {
-        var startTime = Time.time;
-        var endTime = startTime + 5f;
-        var start = 0f;
-        // set end to 1f + normalized width of this objects calculated rect transform width
-        var end = 1f;
-        while (Time.time < endTime)
+        var currentIndex = 0;
+        do
         {
-            var t = (Time.time - startTime) / (endTime - startTime);
-            _scrollRect.horizontalNormalizedPosition = Mathf.Lerp(start, end, t * t * (3f - 2f * t));
-            yield return null;
-        }
+            _textMeshProUGUI.text = GenerateSubstringString(currentIndex);
+            currentIndex += 1;
+            yield return new WaitForSeconds(0.2f);
+            Debug.Log("Scrolling: " + currentIndex + " / " + _textMeshProUGUI.text.Length + ": " + _textMeshProUGUI.text);
+        } while (currentIndex < _fullTrackName.Length - (CharacterMaximum - 1));
+        
+        yield return new WaitForSeconds(1.5f);
+        
+        //
+        // var startTime = Time.time;
+        // var endTime = startTime + 5f;
+        // var start = 0f;
+        // // set end to 1f + normalized width of this objects calculated rect transform width
+        // var end = 1f;
+        // while (Time.time < endTime)
+        // {
+        //     var t = (Time.time - startTime) / (endTime - startTime);
+        //     _scrollRect.horizontalNormalizedPosition = Mathf.Lerp(start, end, t * t * (3f - 2f * t));
+        //     yield return null;
+        // }
         _animator.Play("SlideOut");
     }
 }
